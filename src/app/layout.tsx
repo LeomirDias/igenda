8b61038e73ac1d76cb76041995c8c3ca03bb6e75
LeomirDias <leomirdiasti@outlook.com>
2025-05-28 19:13:26 -0300

    feat: add sign up form

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
