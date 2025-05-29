11f8b919abc815e9576e639abda02608caae0cf3
LeomirDias <leomirdiasti@outlook.com>
2025-05-29 15:07:49 -0300

    feat: add enterprises data to user and render it  on sidebar

--- src/app/(protected)/_components/app-sidebar.tsx
+++ src/app/(protected)/_components/app-sidebar.tsx
@@ -5,7 +5,7 @@ import Image from "next/image"
 import Link from "next/link"
 import { useRouter } from "next/navigation";
 
-import { Button } from "@/components/ui/button"
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
 import {
     Sidebar,
@@ -91,6 +91,8 @@ export function AppSidebar() {
 
     const router = useRouter();
 
+    const session = authClient.useSession();
+
     const handleSignOut = async () => {
         await authClient.signOut({
             fetchOptions: {
@@ -164,7 +166,17 @@ export function AppSidebar() {
                     <SidebarMenuItem>
                         <DropdownMenu>
                             <DropdownMenuTrigger asChild>
-                                <Button>Empresa</Button>
+                                <SidebarMenuButton size="lg">
+                                    <Avatar>
+                                        <AvatarFallback>
+                                            F
+                                        </AvatarFallback>
+                                    </Avatar>
+                                    <div>
+                                        <p className="text-sm">{session.data?.user.enterprise.name}</p>
+                                        <p className="text-sm text-muted-foreground">{session.data?.user.email}</p>
+                                    </div>
+                                </SidebarMenuButton>
                             </DropdownMenuTrigger>
                             <DropdownMenuContent>
                                 <DropdownMenuItem onClick={handleSignOut}>
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,5 +1,7 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { customSession } from "better-auth/plugins";
+import { eq } from "drizzle-orm";
 
 import { db } from "@/db";
 import * as schema from '@/db/schema';
@@ -17,6 +19,28 @@ export const auth = betterAuth({
             clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
         },
     },
+    plugins: [
+        customSession(async ({ user, session }) => {
+            const enterprises = await db.query.usersToEnterprisesTable.findMany({
+                where: eq(schema.usersToEnterprisesTable.userId, user.id),
+                with: {
+                    enterprise: true,
+                },
+            });
+            //Ao adaptar para múltiplas empresas, o usuário pode ter mais de uma empresa associada. Deve-se atualizar a lógica para lidar com isso.
+            const enterprise = enterprises[0];
+            return {
+                user: {
+                    ...user,
+                    enterprise: {
+                        id: enterprise.enterpriseId,
+                        name: enterprise.enterprise.name,
+                    }
+                },
+                session,
+            }
+        })
+    ],
     user: {
         modelName: "usersTable"
     },
