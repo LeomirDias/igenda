66606afc73e68b80e8a0a60df84ebaf8986b9a27
LeomirDias <leomirdiasti@outlook.com>
2025-05-30 10:08:00 -0300

    feat: add ProfessionalCard component and integrate it into ProfessionalsPage

---
+++ src/app/(protected)/professionals/_components/profesisonal-card.tsx
@@ -0,0 +1,68 @@
+"use client";
+import { Calendar1Icon, Clock10Icon } from "lucide-react";
+
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardFooter, CardHeader } from "@/components/ui/card";
+import { Dialog, DialogTrigger } from "@/components/ui/dialog";
+import { Separator } from "@/components/ui/separator";
+import { professionalsTable } from "@/db/schema";
+
+import { getAvailability } from "../helpers/availability";
+import UpsertProfessionalForm from "./upsert-professional-form";
+
+interface ProfessionalCardProps {
+    professional: typeof professionalsTable.$inferSelect
+}
+
+const ProfessionalCard = ({ professional }: ProfessionalCardProps) => {
+
+    const professionalInitials = professional.name
+        .split(" ")
+        .map((name) => name[0])
+        .join("");
+
+    const availability = getAvailability(professional);
+
+    return (
+        <Card>
+            <CardHeader>
+                <div className="flex items-center gap-2">
+                    <Avatar className="h-10 w-10">
+                        <AvatarFallback>{professionalInitials}</AvatarFallback>
+                    </Avatar>
+                    <div>
+                        <h3 className="text-sm font-medium">{professional.name}</h3>
+                        <p className="text-sm text-muted-foreground">
+                            {professional.specialty}
+                        </p>
+                    </div>
+                </div>
+            </CardHeader>
+            <Separator />
+            <CardContent className="flex flex-col gap-2">
+                <Badge variant="outline">
+                    <Calendar1Icon className="mr-1" />
+                    {availability.from.format("dddd")} a {availability.to.format("dddd")}
+                </Badge>
+                <Badge variant="outline">
+                    <Clock10Icon className="mr-1" />
+                    {availability.from.format("HH:mm")} às {availability.to.format("HH:mm")}
+                </Badge>
+            </CardContent>
+            <Separator />
+            <CardFooter>
+                <Dialog>
+                    <DialogTrigger asChild>
+                        <Button className="w-full">Ver detalhes</Button>
+                    </DialogTrigger>
+                    <UpsertProfessionalForm />
+                </Dialog>
+
+            </CardFooter>
+        </Card>
+    );
+}
+
+export default ProfessionalCard;
--- src/app/(protected)/professionals/page.tsx
+++ src/app/(protected)/professionals/page.tsx
@@ -1,10 +1,14 @@
+import { eq } from "drizzle-orm";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
 import { PageActions, PageContainer, PageContent, PageDescription, PageHeader, PageHeaderContent, PageTitle } from "@/components/ui/page-container"
+import { db } from "@/db";
+import { professionalsTable } from "@/db/schema";
 import { auth } from "@/lib/auth";
 
 import AddProfessionalButton from "./_components/add-professional-button";
+import ProfessionalCard from "./_components/profesisonal-card";
 
 const ProfessionalsPage = async () => {
 
@@ -18,6 +22,10 @@ const ProfessionalsPage = async () => {
         redirect("/enterprise-form");
     }
 
+    const professionals = await db.query.professionalsTable.findMany({
+        where: eq(professionalsTable.enterpriseId, session.user.enterprise.id),
+    })
+
     return (
         <PageContainer>
             <PageHeader>
@@ -30,7 +38,9 @@ const ProfessionalsPage = async () => {
                 </PageActions>
             </PageHeader>
             <PageContent>
-                <p>Em breve, você poderá gerenciar os profissionais da sua empresa.</p>
+                <div className="grid grid-cols-3 gap-6">
+                    {professionals.map(professional => <ProfessionalCard key={professional.id} professional={professional} />)}
+                </div>
             </PageContent>
         </PageContainer>
     );
