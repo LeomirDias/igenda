4145926281d86d0524c121f880003beb7bf0bdd0
LeomirDias <leomirdiasti@outlook.com>
2025-06-02 14:21:10 -0300

    feat: ensure products can be deleted

--- src/app/(protected)/products/_components/table-actions.tsx
+++ src/app/(protected)/products/_components/table-actions.tsx
@@ -12,6 +12,20 @@ import { Dialog } from "@/components/ui/dialog";
 import UpsertProductForm from "./upsert-product-form";
 import { productsTable } from "@/db/schema";
 import { useState } from "react";
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+import { useAction } from "next-safe-action/hooks";
+import { deleteProduct } from "@/actions/delete-product";
+import { toast } from "sonner";
 
 interface ProductsTableActionsProps {
     product: typeof productsTable.$inferSelect;
@@ -20,31 +34,69 @@ interface ProductsTableActionsProps {
 const TableProductActions = ({ product }: ProductsTableActionsProps) => {
 
     const [upsertDialgoIsOpen, setUpsertDialgoOpen] = useState(false);
+    const [deleteAlertDialogIsOpen, setDeleteAlertDialogOpen] = useState(false);
+
+    const { execute: executeDeleteProduct, status: deleteProductStatus } = useAction(deleteProduct, {
+        onSuccess: () => {
+            toast.success("Produto excluído com sucesso!");
+            setDeleteAlertDialogOpen(false);
+        },
+        onError: (error) => {
+            toast.error(error.error.serverError || "Erro ao excluir produto.");
+            setDeleteAlertDialogOpen(false);
+        }
+    });
+
+    const handleDelete = () => {
+        executeDeleteProduct({ id: product.id });
+    }
 
     return (
-        <Dialog open={upsertDialgoIsOpen} onOpenChange={setUpsertDialgoOpen}>
-            <DropdownMenu>
-                <DropdownMenuTrigger asChild>
-                    <Button variant="ghost" size="icon">
-                        <MoreVerticalIcon className="w-4 h-4" />
-                    </Button>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent>
-                    <DropdownMenuLabel>Ações para {product.name}</DropdownMenuLabel>
-                    <DropdownMenuSeparator />
-                    <DropdownMenuItem onClick={() => setUpsertDialgoOpen(true)}>
-                        <EditIcon className="w-4 h-4" />
-                        Editar</DropdownMenuItem>
-                    <DropdownMenuItem>
-                        <Trash2 className="w-4 h-4" />
-                        Excluir</DropdownMenuItem>
-                </DropdownMenuContent>
-            </DropdownMenu>
-            <UpsertProductForm
-                product={product}
-                onSuccess={() => setUpsertDialgoOpen(false)}
-            />
-        </Dialog>
+        <>
+            <Dialog open={upsertDialgoIsOpen} onOpenChange={setUpsertDialgoOpen}>
+                <AlertDialog open={deleteAlertDialogIsOpen} onOpenChange={setDeleteAlertDialogOpen}>
+                    <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                            <Button variant="ghost" size="icon">
+                                <MoreVerticalIcon className="w-4 h-4" />
+                            </Button>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent>
+                            <DropdownMenuLabel>Ações para {product.name}</DropdownMenuLabel>
+                            <DropdownMenuSeparator />
+                            <DropdownMenuItem onClick={() => setUpsertDialgoOpen(true)}>
+                                <EditIcon className="w-4 h-4 mr-2" />
+                                Editar
+                            </DropdownMenuItem>
+                            <AlertDialogTrigger asChild>
+                                <DropdownMenuItem>
+                                    <Trash2 className="w-4 h-4 mr-2" />
+                                    Excluir
+                                </DropdownMenuItem>
+                            </AlertDialogTrigger>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                    <UpsertProductForm
+                        product={product}
+                        onSuccess={() => setUpsertDialgoOpen(false)}
+                    />
+                    <AlertDialogContent>
+                        <AlertDialogHeader>
+                            <AlertDialogTitle>Você tem certeza?</AlertDialogTitle>
+                            <AlertDialogDescription>
+                                Esta ação não pode ser desfeita. Isso excluirá permanentemente o produto.
+                            </AlertDialogDescription>
+                        </AlertDialogHeader>
+                        <AlertDialogFooter>
+                            <AlertDialogCancel>Cancelar</AlertDialogCancel>
+                            <AlertDialogAction onClick={handleDelete} disabled={deleteProductStatus === 'executing'}>
+                                {deleteProductStatus === 'executing' ? "Excluindo..." : "Excluir"}
+                            </AlertDialogAction>
+                        </AlertDialogFooter>
+                    </AlertDialogContent>
+                </AlertDialog>
+            </Dialog>
+        </>
     );
 }
 
