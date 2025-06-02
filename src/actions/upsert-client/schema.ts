553bdf3a4358458e8d79eb7b8f43a4038b391ef6
LeomirDias <leomirdiasti@outlook.com>
2025-06-02 09:43:18 -0300

    fix: corrigir mensagem de erro no esquema de validação do cliente

--- src/actions/upsert-client/schema.ts
+++ src/actions/upsert-client/schema.ts
@@ -2,7 +2,7 @@ import z from "zod";
 
 export const upsertClientSchema = z.object({
     id: z.string().uuid().optional(),
-    name: z.string().trim().min(1, { message: "Nome do serviço é obrigatório." }),
+    name: z.string().trim().min(1, { message: "Nome do cliente é obrigatório." }),
     email: z.string().email({ message: "Email inválido." }),
     phoneNumber: z.string().min(1, { message: "Número de telefone é obrigatório." }),
 })
