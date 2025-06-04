719b60d555afcbe4583f0199d640b4fe0bd3b359
LeomirDias <leomirdiasti@outlook.com>
2025-06-04 20:23:25 -0300

    feat: add today-appointments list

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -14,6 +14,10 @@ import { AppointmentsChart } from "./_components/appoitments-chart";
 import TopProfessionals from "./_components/top-professionals";
 import TopSpecialties from "./_components/top-services";
 import TopServices from "./_components/top-services";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Calendar } from "lucide-react";
+import { DataTable } from "@/components/ui/data-table";
+import { appointmentsTableColumns } from "../appointments/_components/table-columns";
 
 interface DashboardPageProps {
     searchParams: Promise<{
@@ -51,6 +55,7 @@ const DashboardPage = async ({ searchParams }: DashboardPageProps) => {
         [totalProfessionals],
         topProfessionals,
         topServices,
+        todayAppointments,
     ] = await Promise.all([
         db.select({
             total: sum(appointmentsTable.appointmentPriceInCents),
@@ -133,6 +138,20 @@ const DashboardPage = async ({ searchParams }: DashboardPageProps) => {
             .groupBy(servicesTable.id)
             .orderBy(desc(count(servicesTable.name)))
             .limit(5),
+
+        db.query.appointmentsTable.findMany({
+            where: and(
+                eq(appointmentsTable.enterpriseId, session.user.enterprise.id),
+                gte(appointmentsTable.date, new Date(from)),
+                lte(appointmentsTable.date, new Date(to)),
+            ),
+            with: {
+                client: true,
+                professional: true,
+                service: true,
+            }
+        })
+
     ]);
 
     const chartStartDate = dayjs().subtract(10, 'days').startOf('day').toDate();
@@ -175,10 +194,26 @@ const DashboardPage = async ({ searchParams }: DashboardPageProps) => {
                 />
             </PageContent>
             <div className="grid grid-cols-[2.25fr_1fr] gap-4">
-                <AppointmentsChart dailyAppointmentsData={dailyAppointmentsData} />
+                <Card>
+                    <CardHeader>
+                        <div className="flex items-center gap-3">
+                            <Calendar className="text-muted-foreground" />
+                            <CardTitle className="text-base">
+                                Agendamentos de hoje
+                            </CardTitle>
+                            <CardDescription>
+                                Aqui estão os agendamentos de hoje
+                            </CardDescription>
+                        </div>
+                    </CardHeader>
+                    <CardContent>
+                        <DataTable columns={appointmentsTableColumns} data={todayAppointments} />
+                    </CardContent>
+                </Card>
                 <TopProfessionals professionals={topProfessionals} />
             </div>
             <div className="grid grid-cols-[2.25fr_1fr] gap-4">
+                <AppointmentsChart dailyAppointmentsData={dailyAppointmentsData} />
                 <TopServices topServices={topServices} />
             </div>
 
