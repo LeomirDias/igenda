995b7cae29f4deeaed1a4da03b5e6081e01599a0
LeomirDias <leomirdiasti@outlook.com>
2025-06-05 20:01:04 -0300

    feat: add cover and logo images on login page and rearrange layout

--- src/app/authentication/page.tsx
+++ src/app/authentication/page.tsx
@@ -19,25 +19,38 @@ const AuthenticationPage = async () => {
     }
 
     return (
-        <div className="flex h-screen w-screen items-center justify-center">
+        <div className="flex h-screen w-screen">
+            <div className="w-1/2 h-full">
+                <img src="/CapaAuthentication.png" alt="logo" className="h-full w-full object-cover" />
+            </div>
 
-            <Tabs defaultValue="login" className="w-[400px]">
-                <TabsList className="grid w-full grid-cols-2">
-                    <TabsTrigger value="login">Login</TabsTrigger>
-                    <TabsTrigger value="register">Criar conta</TabsTrigger>
-                </TabsList>
+            <div className="w-1/2 flex flex-col">
+                <div className="flex-1 flex items-center justify-center">
+                    <div className="w-[400px]">
+                        <Tabs defaultValue="login" className="w-full">
+                            <TabsList className="grid w-full grid-cols-2">
+                                <TabsTrigger value="login">Login</TabsTrigger>
+                                <TabsTrigger value="register">Criar conta</TabsTrigger>
+                            </TabsList>
 
-                <TabsContent value="login">
-                    <LoginForm />
-                </TabsContent>
+                            <TabsContent value="login">
+                                <LoginForm />
+                            </TabsContent>
 
-                <TabsContent value="register">
-                    <SignUpForm />
-                </TabsContent>
-
-            </Tabs>
+                            <TabsContent value="register">
+                                <SignUpForm />
+                            </TabsContent>
+                        </Tabs>
+                    </div>
+                </div>
+                <footer className="w-[400px] py-4 text-center text-sm text-muted-foreground mx-auto mb-8">
+                    © {new Date().getFullYear()} iGenda. Todos os direitos reservados.
+                </footer>
+            </div>
         </div>
     )
 };
+
+
 
 export default AuthenticationPage;
