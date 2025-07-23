ac652ce31d531cbe0f3763a9b8bd0be062ffa399
LeomirDias <leomirdiasti@outlook.com>
2025-07-23 15:11:32 -0300

    refactor: add status field to appointments table and update createAppointment action to set default status to 'scheduled'

--- src/actions/create-appointments/index.ts
+++ src/actions/create-appointments/index.ts
@@ -12,49 +12,50 @@ import { getAvailableTimes } from "../get-available-times";
 import { createAppointmentSchema } from "./schema";
 
 export const createAppointment = actionClient
-    .schema(createAppointmentSchema)
-    .action(async ({ parsedInput }) => {
-        const availableTimes = await getAvailableTimes({
-            professionalId: parsedInput.professionalId,
-            date: dayjs(parsedInput.date).format("YYYY-MM-DD"),
-        });
-
-        if (!availableTimes?.data) {
-            throw new Error("No available times");
-        }
-
-        const isTimeAvailable = availableTimes.data?.some(
-            (time) => time.value === parsedInput.time && time.available,
-        );
-
-        if (!isTimeAvailable) {
-            throw new Error("Time not available");
-        }
-
-        // Busca o preço do serviço selecionado
-        const service = await db.query.servicesTable.findFirst({
-            where: eq(servicesTable.id, parsedInput.serviceId),
-        });
-
-        if (!service) {
-            throw new Error("Service not found");
-        }
-
-        const appointmentDateTime = dayjs(parsedInput.date)
-            .set("hour", parseInt(parsedInput.time.split(":")[0]))
-            .set("minute", parseInt(parsedInput.time.split(":")[1]))
-            .toDate();
-
-        await db.insert(appointmentsTable).values({
-            clientId: parsedInput.clientId,
-            serviceId: parsedInput.serviceId,
-            professionalId: parsedInput.professionalId,
-            time: parsedInput.time,
-            date: appointmentDateTime,
-            enterpriseId: parsedInput.enterpriseId,
-            appointmentPriceInCents: service.servicePriceInCents,
-        });
-
-        revalidatePath("/appointments");
-        revalidatePath("/dashboard");
+  .schema(createAppointmentSchema)
+  .action(async ({ parsedInput }) => {
+    const availableTimes = await getAvailableTimes({
+      professionalId: parsedInput.professionalId,
+      date: dayjs(parsedInput.date).format("YYYY-MM-DD"),
     });
+
+    if (!availableTimes?.data) {
+      throw new Error("No available times");
+    }
+
+    const isTimeAvailable = availableTimes.data?.some(
+      (time) => time.value === parsedInput.time && time.available,
+    );
+
+    if (!isTimeAvailable) {
+      throw new Error("Time not available");
+    }
+
+    // Busca o preço do serviço selecionado
+    const service = await db.query.servicesTable.findFirst({
+      where: eq(servicesTable.id, parsedInput.serviceId),
+    });
+
+    if (!service) {
+      throw new Error("Service not found");
+    }
+
+    const appointmentDateTime = dayjs(parsedInput.date)
+      .set("hour", parseInt(parsedInput.time.split(":")[0]))
+      .set("minute", parseInt(parsedInput.time.split(":")[1]))
+      .toDate();
+
+    await db.insert(appointmentsTable).values({
+      clientId: parsedInput.clientId,
+      serviceId: parsedInput.serviceId,
+      professionalId: parsedInput.professionalId,
+      time: parsedInput.time,
+      date: appointmentDateTime,
+      enterpriseId: parsedInput.enterpriseId,
+      appointmentPriceInCents: service.servicePriceInCents,
+      status: "scheduled",
+    });
+
+    revalidatePath("/appointments");
+    revalidatePath("/dashboard");
+  });
