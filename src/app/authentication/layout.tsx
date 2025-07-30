25835237d8db512387e05510892a19110f9813ad
LeomirDias <leomirdiasti@outlook.com>
2025-07-30 08:33:00 -0300

    fix: update title in AuthenticationLayout for consistency and improved branding

--- src/app/authentication/layout.tsx
+++ src/app/authentication/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "iGenda | Autenticação",
+  title: "iGenda - Autenticação",
 };
 
 export default function AuthenticationLayout({
